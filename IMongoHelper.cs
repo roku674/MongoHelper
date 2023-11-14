84df98dc8769dd729079e218d49e8cd87d0bc574
Alexander Fields <Roku674@gmail.com>
2023-11-13 19:28:32 -0500

    -a

---
+++ IMongoHelper.cs
@@ -0,0 +1,93 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Optimization.Repository
+{
+    /// <summary>
+    /// Make sure you set the MongoDB Instance before calling the classes in this 
+    /// </summary>
+
+    public interface IMongoHelper
+    {
+        /// <summary>
+        /// The Databaes
+        /// </summary>
+        IMongoDatabase database { get; set; }
+
+        /// <summary>
+        /// db name for easier eaccess
+        /// </summary>
+        public string dbName { get; set; }
+
+
+        /// <summary>
+        /// Add a Document to MongoDB
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbName"></param>
+        /// <param name="collectionName"></param>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        Task CreateDocumentAsync<T>(string dbName, string collectionName, T document);
+        /// <summary>
+        /// Create Instance of the Database
+        /// </summary>
+        /// <param name="dbName"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        IMongoDatabase CreateMongoDbInstance(string dbName, string connectionString);
+        /// <summary>
+        /// Be Careful
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbName"></param>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task DeleteDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter);
+        /// <summary>
+        /// Get All documents in collection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbName"></param>
+        /// <param name="collectionName"></param>
+        /// <returns></returns>
+        Task<List<T>> GetAllDocumentsAsync<T>(string dbName, string collectionName);
+        /// <summary>
+        /// Get a Mongo Document
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbName"></param>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task<List<T>> GetFilteredDocumentsAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter);
+        /// <summary>
+        /// Tests the connection to the database
+        /// </summary>
+        /// <returns>Will return a List of Collection Names if it worked otherwise returns null</returns>
+        List<string> TestConnection();
+        /// <summary>
+        /// Updates a document
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbName"></param>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <param name="updateDefinition"></param>
+        /// <returns></returns>
+        Task UpdateDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter, UpdateDefinition<T> updateDefinition);
+
+        /// <summary>
+        /// Replaces a document
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dbName"></param>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        Task UpdateDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter, T document);
+    }
+}
---
+++ MongoHelper.cs
@@ -0,0 +1,148 @@
+﻿using APIBilling;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Optimization.Repository
+{
+    /// <summary>
+    /// Make sure you set the MongoDB Instance before calling the classes in this 
+    /// </summary>
+    public class MongoHelper : IMongoHelper
+    {
+        public IMongoDatabase database { get; set; }
+
+        public string dbName { get; set; }
+
+        /// <summary>
+        /// default Constrcutor
+        /// </summary>
+        public MongoHelper()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor that will get Database
+        /// </summary>
+        /// <param name="dbName"></param>
+        /// <param name="connectionString"></param>
+        public MongoHelper(string dbName, string connectionString)
+        {
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase(dbName);
+            database = db;
+        }
+
+        /// <summary>
+        /// Helper for connecting to database
+        /// </summary>
+        /// <param name="mongoHelper">instance of the mongo helper that will connect</param>
+        /// <param name="dbName"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="cluster"></param>
+        /// <returns>mongo helper</returns>
+        public static IMongoHelper MongoHelperConnector(IMongoHelper mongoHelper, string dbName, string username, string password, string cluster)
+        {
+            string connectionString = ConnectionStringBuilder(username, password, cluster);
+
+            mongoHelper.database = mongoHelper.CreateMongoDbInstance(dbName, connectionString);
+            try
+            {
+                List<string> collections = mongoHelper.TestConnection();
+            }
+            catch (System.Exception theseHands)
+            {
+                System.Console.WriteLine(theseHands.ToString());
+            }
+
+            mongoHelper.dbName = dbName;
+
+            return mongoHelper;
+        }
+
+        /// <summary>
+        /// Connection string builder
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="cluster"></param>
+        /// <returns></returns>
+        public static string ConnectionStringBuilder(string username, string password, string cluster)
+        {
+            string encodedPassword = System.Net.WebUtility.UrlEncode(password);
+
+            //string connectionString = $"mongodb+srv://{username}:{encodedPassword}@{cluster}.vc4onns.mongodb.net/?retryWrites=true&w=majority";
+            string connectionString = $"mongodb+srv://{username}:{encodedPassword}@{cluster}.vpazkca.mongodb.net/?retryWrites=true&w=majority";
+            return connectionString;
+        }
+
+        /// <summary>
+        /// For if already constructed to get Database
+        /// </summary>
+        /// <param name="dbName"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public IMongoDatabase CreateMongoDbInstance(string dbName, string connectionString)
+        {
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase(dbName);
+            return db;
+        }
+
+        /// <summary>
+        /// Tests the connection to the database
+        /// </summary>
+        /// <returns>Will return a List of Collection Names if it worked otherwise returns null</returns>
+        public List<string> TestConnection()
+        {
+            try
+            {
+                List<string> collectionNames = database.ListCollectionNames().ToList();
+                return collectionNames;
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine("Was unable to properly connect to the database!" + e);
+                return null;
+            }
+        }
+
+        public async Task<List<T>> GetAllDocumentsAsync<T>(string dbName, string collectionName)
+        {
+            IMongoCollection<T> collection = GetCollection<T>(dbName, collectionName);
+            return await collection.Find(x => true).ToListAsync();
+        }
+
+        public async Task<List<T>> GetFilteredDocumentsAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter)
+        {
+            return await GetCollection<T>(dbName, collectionName).Find(filter).ToListAsync();
+        }
+
+        public async Task UpdateDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter, T document)
+        {
+            await GetCollection<T>(dbName, collectionName).ReplaceOneAsync(filter, document);
+        }
+        public async Task UpdateDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter, UpdateDefinition<T> document)
+        {
+            await GetCollection<T>(dbName, collectionName).UpdateOneAsync(filter, document);
+        }
+
+        public async Task CreateDocumentAsync<T>(string dbName, string collectionName, T document)
+        {
+            await GetCollection<T>(dbName, collectionName).InsertOneAsync(document);
+        }
+
+        public async Task DeleteDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter)
+        {
+            await GetCollection<T>(dbName, collectionName).DeleteOneAsync(filter);
+        }
+
+        private IMongoCollection<T> GetCollection<T>(string dbName, string collectionName)
+        {
+            return database.GetCollection<T>(collectionName);
+        }
+
+    }
+}
