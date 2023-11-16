414cd1d959d4252de69384d01169a781dbd35490
Alexander Fields <Roku674@gmail.com>
2023-11-15 20:41:06 -0500

    Update IMongoHelper.cs

--- IMongoHelper.cs
+++ IMongoHelper.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Driver;
+using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -20,16 +20,14 @@ namespace Optimization.Repository
         /// </summary>
         public string dbName { get; set; }
 
-
         /// <summary>
         /// Add a Document to MongoDB
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="dbName"></param>
         /// <param name="collectionName"></param>
         /// <param name="document"></param>
         /// <returns></returns>
-        Task CreateDocumentAsync<T>(string dbName, string collectionName, T document);
+        Task CreateDocumentAsync<T>(string collectionName, T document);
         /// <summary>
         /// Create Instance of the Database
         /// </summary>
@@ -41,28 +39,25 @@ namespace Optimization.Repository
         /// Be Careful
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="dbName"></param>
         /// <param name="collectionName"></param>
         /// <param name="filter"></param>
         /// <returns></returns>
-        Task DeleteDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter);
+        Task DeleteDocumentAsync<T>(string collectionName, FilterDefinition<T> filter);
         /// <summary>
         /// Get All documents in collection
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="dbName"></param>
         /// <param name="collectionName"></param>
         /// <returns></returns>
-        Task<List<T>> GetAllDocumentsAsync<T>(string dbName, string collectionName);
+        Task<List<T>> GetAllDocumentsAsync<T>(string collectionName);
         /// <summary>
         /// Get a Mongo Document
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="dbName"></param>
         /// <param name="collectionName"></param>
         /// <param name="filter"></param>
         /// <returns></returns>
-        Task<List<T>> GetFilteredDocumentsAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter);
+        Task<List<T>> GetFilteredDocumentsAsync<T>(string collectionName, FilterDefinition<T> filter);
         /// <summary>
         /// Tests the connection to the database
         /// </summary>
@@ -72,22 +67,20 @@ namespace Optimization.Repository
         /// Updates a document
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="dbName"></param>
         /// <param name="collectionName"></param>
         /// <param name="filter"></param>
         /// <param name="updateDefinition"></param>
         /// <returns></returns>
-        Task UpdateDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter, UpdateDefinition<T> updateDefinition);
+        Task UpdateDocumentAsync<T>(string collectionName, FilterDefinition<T> filter, UpdateDefinition<T> updateDefinition);
 
         /// <summary>
         /// Replaces a document
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="dbName"></param>
         /// <param name="collectionName"></param>
         /// <param name="filter"></param>
         /// <param name="document"></param>
         /// <returns></returns>
-        Task UpdateDocumentAsync<T>(string dbName, string collectionName, FilterDefinition<T> filter, T document);
+        Task UpdateDocumentAsync<T>(string collectionName, FilterDefinition<T> filter, T document);
     }
 }
